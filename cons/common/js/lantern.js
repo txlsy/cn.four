d7b172c6feda97147467706f22bf1914848a4c78
lsy <334224109@qq.com>
2015-04-16 00:18:06 +0800

    实现图片轮播

--- cons/common/js/lantern.js
+++ cons/common/js/lantern.js
@@ -1,3 +1,13 @@
-function lantern (id) {
-		$('#'+id)
+var lantern = function (id) {
+	lantern.len = $("."+id).children("li").length;
+	lantern.cur = 2;
+	var i = setInterval(function(){tinkle(id)},1000);
+}
+
+function tinkle(id) {
+	$('.'+id).children('li:eq(' + lantern.cur + ')').removeClass('top');
+	$('.'+id).children('li').addClass('below');
+	lantern.cur = (lantern.cur+1)%lantern.len;
+	console.info(lantern.cur);
+	$('.'+id).children('li:eq(' + lantern.cur + ')').addClass('top');
 }

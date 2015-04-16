89ff38944ea18781595dc3415719abb168bf2c6d
mark <334224109@qq.com>
2015-04-16 18:34:36 +0800

    small change to lantern();

--- cons/common/js/lantern.js
+++ cons/common/js/lantern.js
@@ -1,13 +1,14 @@
 var lantern = function (id) {
+	lantern.sel = id;
 	lantern.len = $("."+id).children("li").length;
 	lantern.cur = 2;
-	var i = setInterval(function(){tinkle(id)},1000);
+	var i = setInterval("tinkle()",1000);
 }
 
-function tinkle(id) {
+function tinkle() {
+	var id = lantern.sel;
 	$('.'+id).children('li:eq(' + lantern.cur + ')').removeClass('top');
 	$('.'+id).children('li').addClass('below');
 	lantern.cur = (lantern.cur+1)%lantern.len;
-	console.info(lantern.cur);
 	$('.'+id).children('li:eq(' + lantern.cur + ')').addClass('top');
 }

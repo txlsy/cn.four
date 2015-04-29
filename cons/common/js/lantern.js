27b9c8659289c8b2add4ab05a64cbc7ef71e2a32
mark <334224109@qq.com>
2015-04-29 23:52:19 +0800

    jumping func completed

--- cons/common/js/lantern.js
+++ cons/common/js/lantern.js
@@ -1,21 +1,55 @@
 var lantern = function (id) {
 	lantern.sel = id;
-	lantern.len = $("."+id).children("li").length;
-	lantern.cur = 2;
-	createArrow();
-	var i = setInterval("tinkle()",3000);
+	init();
 }
 
-function tinkle() {
+function init(){
 	var id = lantern.sel;
-	$('.'+id).children('li:eq(' + lantern.cur + ')').removeClass('top');
-	$('.'+id).children('li').addClass('below');
-	lantern.cur = (lantern.cur+1)%lantern.len;
+	lantern.len = $("."+id).children("li").length;
+	lantern.cur = 0;
+	createArrow();
+	bindJump();
 	$('.'+id).children('li:eq(' + lantern.cur + ')').addClass('top');
+	lantern.intv = setInterval("twinkle()",3000);
+}
+
+function twinkle(){
+	kaeri();
+	setCurrent(1);
+	kaeri();
+}
+
+function setCurrent(change){
+	change %= lantern.len;
+	change += lantern.len;
+	change %= lantern.len;
+	lantern.cur = (lantern.cur+change)%lantern.len;
+}
+
+function kaeri() {
+	var id = lantern.sel;
+	$('.'+id).children('li:eq(' + lantern.cur + ')').toggleClass('top');
 }
 
 function createArrow(){
 	var id = lantern.sel;
 	$('.'+id).append('<div class="left-arrow">&lt;</div>');
 	$('.'+id).append('<div class="right-arrow">&gt;</div>');
+}
+
+function bindJump(){
+	$('.left-arrow').click(function(){
+		jump(-1);
+	});
+	$('.right-arrow').click(function(){
+		jump(1);
+	});
+}
+
+function jump(muke){
+	window.clearInterval(lantern.intv);
+	kaeri();
+	setCurrent(muke);
+	kaeri();
+	lantern.intv = setInterval("twinkle()",3000);
 }
